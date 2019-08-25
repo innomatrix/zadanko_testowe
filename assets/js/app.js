3a511847e0c6a59adccac873192ea1ca0811b46e
innomatrix <victor.depta@outlook.com>
2019-08-25 16:42:25 +0200

    cleaning yarn fotn-awsome

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,13 +7,6 @@ import axios from 'axios'
 import VueLoading from "./components/layout/overlay"
 import VueGeolocation from 'vue-browser-geolocation'
 import {ServerTable, Event} from 'vue-tables-2';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-library.add(faUserSecret)
-
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 require('./app.scss');
 
